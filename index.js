9269d5636543917f83c9479d534ec8effa1506db
Andi Gu <andi.gu.ca@gmail.com>
2018-02-04 10:07:09 -0500

    Resolved

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import React from 'react'
 import { AppRegistry } from 'react-native'
 import App from './App'
 
-<<<<<<< HEAD
-=======
+
 console.disableYellowBox = true
 
->>>>>>> cd18da8f86e75fe89c705c6b40ca79792231ab06
 AppRegistry.registerComponent('QReceipts', () => App)
